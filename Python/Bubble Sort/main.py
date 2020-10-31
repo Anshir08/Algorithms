42e2bd85ea38a42d673eff46a0d23afe89019b1a
Anshir08 <69457505+Anshir08@users.noreply.github.com>
2020-10-31 11:11:09 -0700

    Update main.py

--- Python/Bubble Sort/main.py
+++ Python/Bubble Sort/main.py
@@ -1,23 +1,7 @@
-
-def bubble_sort(list_to_sort):
-    """ Sort list in input using bubble sorting.
-
-    :param list_to_sort: the list to sort
-    :type list_to_sort: list
-    :return: the list sorted
-    :rtype: list
-    """
-    list_size = len(list_to_sort)
-    for i in range(list_size):
-        for j in range(0, list_size-i-1):
-            if list_to_sort[j] > list_to_sort[j+1] :
-                # Swap current and next element
-                list_to_sort[j], list_to_sort[j+1] = list_to_sort[j+1], list_to_sort[j]
-    return list_to_sort
-
-
-# Test sorting method
-n = int(input('Enter size of the list'))
-unsorted_list = [int(input(i) for i in range(n)]
-sorted_list = bubble_sort(unsorted_list)
-print (f"Sorted list is: {', '.join([str(n) for n in sorted_list])}")
+#Bubble Sort
+a = [5, 4, 8, 3, 4, 14, 90, 45, 9, 3, 2, 4]
+for i in range(len(a), 0, -1):
+    for j in range(0, i - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+print(a)
