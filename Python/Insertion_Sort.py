f4ecdf32ad305ab00caf7439adef650b66dc361d
Anshir08 <69457505+Anshir08@users.noreply.github.com>
2020-10-31 10:33:07 -0700

    shell sort and insertion sort
    hey there have a look
    i have added two more sorts in your repo...

---
+++ Python/Insertion_Sort.py
@@ -0,0 +1,9 @@
+a = [5, 4, 8, 3, 4, 14, 90, 45, 9, 3, 2, 4]
+for j in range(1, len(a)):
+    key = a[j]
+    i = j - 1
+    while i > -1 and a[i] > key:
+        a[i + 1] = a[i]
+        i = i - 1
+    a[i + 1] = key
+print(a)
